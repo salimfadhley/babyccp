eacc55b586423498aa23386b557b2f654bba3ca2
Salim Fadhley <salimfadhley@gmail.com>
2019-05-15 15:20:08 +0100

    Added some video stuff & styling. Added some more build configuration.

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,13 @@
 FROM node AS node_base
 RUN npm install -g create-react-app nodemon typescript ts-node
+
+FROM node_base AS node_builder
+COPY /src /src
+WORKDIR /src/wallboard
+RUN rm -rf ./src/wallboard/build
+RUN yarn build
+
+FROM httpd:latest AS web_server
+COPY ./src/wallboard/build /usr/local/apache2/htdocs/
+EXPOSE 80
+EXPOSE 8080
--- src/wallboard/src/App.tsx
+++ src/wallboard/src/App.tsx
@@ -29,12 +29,13 @@ const App: React.FC = () => {
     <div className="App">
 
       <video autoPlay muted loop id="myVideo">
-        <source src="bs_marc.mp4" type="video/mp4" />
+        <source src="bs_montage.mp4" type="video/mp4" />
       </video>
 
         <div className="content">
-            <h1>Heading</h1>
-            <p>Lorem ipsum...</p>
+            <h1>Welcome to Big Smoke Comedy</h1>
+            <p>London's most central comedy club, above <a href="http://www.nellofolddrury.com/">The Nell of Old Drury</a>, one of London's oldest pubs.</p>
+            <p>We are still working on our website, but in the meantime, <a href="https://www.facebook.com/BigSmokeComedy/">visit our page on Facebook</a>.</p>
         </div>
 
     </div>
