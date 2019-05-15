271643d2a82468894a92077f25389c9bdabf33c4
Salim Fadhley <salimfadhley@gmail.com>
2019-05-15 11:49:52 +0100

    Added some video stuff & styling.

--- src/wallboard/src/App.tsx
+++ src/wallboard/src/App.tsx
@@ -4,24 +4,39 @@ import {Counter} from './Counter';
 import './App.css';
 
 
+interface FooInterface {
+  bar: number
+  boo ?: number
+}
+
+interface BarInterface {
+
+}
+
+const Foo: React.FC<FooInterface> = props => {
+  return (<div className="Foo">This is the Foo Widget. Your message is {props.bar}</div>)
+}
+
+
+
+const Bar: React.FC<BarInterface> = (props) => {
+    return (<div className="Foo">Your message is {props.children}</div>)
+}
+
+
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload....!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p><Counter>Hello!</Counter></p>
-      </header>
+
+      <video autoPlay muted loop id="myVideo">
+        <source src="bs_marc.mp4" type="video/mp4" />
+      </video>
+
+        <div className="content">
+            <h1>Heading</h1>
+            <p>Lorem ipsum...</p>
+        </div>
+
     </div>
   );
 }
