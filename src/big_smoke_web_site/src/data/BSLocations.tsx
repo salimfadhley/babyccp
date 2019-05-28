6de5b72f171882a3b2d632b713a975e9815a88fe
Salim Fadhley <salimfadhley@gmail.com>
2019-05-28 12:41:15 +0100

    delete some stuff

--- src/big_smoke_web_site/src/data/BSLocations.tsx
+++
@@ -1,6 +0,0 @@
-import {BSLocation, BSLocationInterface} from "../model/BSLocation";
-
-export const THE_NELL_OF_OLD_DRURY:BSLocationInterface = new BSLocation(
-    "The Nell of Old Drury",
-    "29 Catherine Street, London"
-)
